510f95a98b6be4e01dac8d7d366c85d8b5a0e26f
Sanidhya Koranne <sanidhyakoranne123@gmail.com>
2023-04-03 23:45:49 +0530

    create vite project and components

---
+++ newsapp/src/components/cardcomponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Cardcomponent = () => {
+  return (
+    <div>hey i am cardcomponent</div>
+  )
+}
+
+export default Cardcomponent;
---
+++ newsapp/src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Cardcomponent from './components/cardcomponent'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  
+    <Cardcomponent />,
+)
